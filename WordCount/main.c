08fb153ed3611e17cf57db70f6864c196dec876c
Jy-Feng <JyFenglx@163.com>
2021-05-08 11:00:29 +0800

    first commit

---
+++ WordCount/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  WordCount
+//
+//  Created by bright on 2021/5/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
